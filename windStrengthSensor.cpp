c82e233d911b77a11666bdf9ba6d423e01c027fe
SCSplus <49127621+SCSplus@users.noreply.github.com>
2019-04-18 21:47:44 +0200

    Add files via upload

--- windStrengthSensor.cpp
+++ windStrengthSensor.cpp
@@ -1,5 +1,7 @@
 #include "windStrengthSensor.h"
+#include <iostream>
 
+using namespace std;
 
 
 windStrengthSensor::windStrengthSensor()
@@ -11,7 +13,25 @@ windStrengthSensor::~windStrengthSensor()
 {
 }
 
+int imp_per_sec = 0;
+const int pinNumber = 29;
+
+void windInterrupt(void) {
+	imp_per_sec++;
+	cout << imp_per_sec;
+}
+
 float windStrengthSensor::GetSensorData()
 {
-    return 0.0f;
+	wiringPiSetup();
+	
+	pinMode(pinNumber, INPUT);
+	pullUpDnControl(pinNumber, PUD_UP);
+
+
+	wiringPiISR(pinNumber, INT_EDGE_RISING, &windInterrupt);
+
+	return imp_per_sec;
+
 }
+
--- windStrengthSensor.h
+++ windStrengthSensor.h
@@ -11,30 +11,4 @@ public:
     float GetSensorData();
 };
 
-int imp_per_sec = 0;
-const int pinNumber = 29;
-
-void windInterrupt(void) {
-	imp_per_sec++;
-	std::cout << imp_per_sec;
-}
-
-float windStrengthSensor::GetSensorData()
-{
-	wiringPiSetup();
-
-	pinMode(pinNumber, INPUT);
-	pullUpDnControl(pinNumber, PUD_UP);
-
-
-	float actual_windspeed_msec = 0.0;
-
-
-	wiringPiISR(pinNumber, INT_EDGE_RISING, &windInterrupt);
-
-	std::cout << actual_windspeed_msec;
-	return imp_per_sec;
-
-}
-
 
